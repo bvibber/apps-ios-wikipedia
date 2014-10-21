70cd910d375be38ca914fcf7401c9e6e392053ce
Brion Vibber <brion@pobox.com>
2014-10-21 16:09:11 -0700

    initial attempt to save something \o/
    Change-Id: I2bb6daa835638fb15bc9a3f87cb57d09551cadaf

--- MediaWikiKit/MediaWikiKitTests/MWKDataStoreStorageTests.m
+++ MediaWikiKit/MediaWikiKitTests/MWKDataStoreStorageTests.m
@@ -16,6 +16,8 @@
     MWKTitle *title;
     NSDictionary *json;
     MWKArticle *article;
+
+    NSString *basePath;
     MWKDataStore *dataStore;
 }
 
@@ -31,11 +33,38 @@
     json = [self loadJSON:@"section0"];
     article = [[MWKArticle alloc] initWithTitle:title dict:json[@"mobileview"]];
     
-    dataStore = [[MWKDataStore alloc] initWithBasePath:@"/"];
+    NSString *documentsFolder = [NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES) firstObject];
+    basePath = [documentsFolder stringByAppendingPathComponent:@"unit-test-data"];
+
+    dataStore = [[MWKDataStore alloc] initWithBasePath:basePath];
 }
 
 - (void)tearDown {
     [super tearDown];
+    
+    [[NSFileManager defaultManager] removeItemAtPath:basePath error:nil];
+}
+
+- (void)testWriteReadFile
+{
+    NSString *path = [dataStore pathForArticle:article];
+    [[NSFileManager defaultManager] createDirectoryAtPath:[path stringByDeletingLastPathComponent]
+                              withIntermediateDirectories:YES
+                                               attributes:nil
+                                                    error:nil];
+
+    XCTAssertFalse([[NSFileManager defaultManager] fileExistsAtPath:path], @"file does not exist before we save it");
+
+    NSDictionary *articleExport = [article dataExport];
+    [articleExport writeToFile:path atomically:YES];
+
+    XCTAssert([[NSFileManager defaultManager] fileExistsAtPath:path], @"file does exist after we wrote it");
+
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:path];
+    XCTAssertNotNil(dict);
+    
+    MWKArticle *article2 = [[MWKArticle alloc] initWithTitle:title dict:dict];
+    XCTAssertEqualObjects(article, article2);
 }
 
 @end
