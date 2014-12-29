0b8964c6077566a3ec4a0d5d03b799b8479a6b36
Brion Vibber <brion@pobox.com>
2014-12-29 12:00:48 -0800

    work in progress
    Change-Id: I433d4dbadfa69c1c55ff93e5c3f1576d629cfd61

--- OldDataSchema/OldDataSchema/OldDataSchema.m
+++ OldDataSchema/OldDataSchema/OldDataSchema.m
@@ -29,4 +29,64 @@
     // TODO
 }
 
+-(void)migrateArticle:(Article *)article
+{
+    NSDictionary *dict = [self exportArticle:article];
+    [self.delegate oldDataSchema:self migrateHistoryEntry:dict];
+}
+
+-(NSDictionary *)exportArticle:(Article *)article
+{
+    NSMutableDictionary *dict = [@{} mutableCopy];
+    
+    dict[@"redirected"] = // optional
+    dict[@"lastmodified"] = // date
+    dict[@"lastmodifiedby"] = // dict structure for user or empty
+    dict[@"id"] = // number
+    dict[@"languagecount"] = // number
+    dict[@"displaytitle"] = // optional
+    dict[@"protection"] = // struct
+    dict[@"editable"] = // boolean
+    
+    // sections!
+    dict[@"sections"] = [@[] mutableCopy];
+    for (Section *section in article.section) {
+        int sectionId = [section.sectionId intValue];
+        dict[@"sections"][sectionId] = [self exportSection:section];
+    }
+    
+    return @{
+             @"language": article.domain,
+             @"title": article.title,
+             @"dict": dict
+             };
+}
+
+-(NSDictionary *)exportSection:(Section *)section
+{
+    NSMutableDictionary *dict = [@{} mutableCopy];
+    
+    if (section.tocLevel) {
+        dict[@"toclevel"] = section.tocLevel;
+    }
+    if (section.level) {
+        dict[@"level"] = section.level;
+    }
+    if (section.title) {
+        dict[@"line"] = section.title;
+    }
+    if (section.fromTitle) {
+        dict[@"fromtitle"] = section.fromTitle;
+    }
+    if (section.anchor) {
+        dict[@"anchor"] = section.anchor;
+    }
+    dict[@"id"] = section.sectionId;
+    if (section.html) {
+        dict[@"text"] = section.html;
+    }
+    
+    return dict;
+}
+
 @end
